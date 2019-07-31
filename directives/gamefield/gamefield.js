9a07569a9ae5d904a983f8ce5d5dbf430617b95c
grabowski-michal <44712372+grabowski-michal@users.noreply.github.com>
2019-07-31 18:01:51 +0200

    Add files via upload

---
+++ directives/gamefield/gamefield.js
@@ -0,0 +1,117 @@
+﻿var gamefieldController = app.controller('gamefieldController', function ($scope, $location, $interval, $timeout, $rootScope) {
+    $scope.cardsCount = [0, 1, 2, 3];
+    $scope.tabCard = [];
+    $scope.lastChoice;
+    $scope.numberOfPairs = 0;
+    $scope.pairs = [];
+    $scope.block = false;
+    $rootScope.message = "";
+    $rootScope.mesVisible = false;
+
+    $scope.back = function () {
+        $scope.mesVisible = false;
+        reset();
+        $location.path("/");
+    }
+
+    $rootScope.assemble = function() {
+        $scope.message = $rootScope.message;
+        $scope.mesVisible = $rootScope.mesVisible;
+    }
+
+    $rootScope.reset = reset;
+
+    function reset() {
+        $scope.numberOfPairs = 0;
+        $scope.pairs = [];
+        $scope.lastChoice = undefined;
+        $scope.tabCard = [];
+        $scope.block = false;
+        $rootScope.message = "";
+        $rootScope.mesVisible = false;
+    }
+
+    function generateCards() {
+        var tabTemp = [];
+        for (var i = 1; i <= 8; i++) {
+            for (var j = 0; j < 2; j++) tabTemp.push(i);
+        }
+        for (var i = tabTemp.length; i > 1; i--) {
+            var j = Math.floor(Math.random() * i);
+            var temp = tabTemp[i - 1];
+            tabTemp[i - 1] = tabTemp[j];
+            tabTemp[j] = temp;
+        }
+
+        var allTab = [];
+        for (var i = 0; i < 4; i++) {
+            var line = [];
+            for (var j = 0; j < 4; j++) line.push(tabTemp[i * 4 + j]);
+            allTab.push(line);
+        }
+        return allTab;
+    }
+
+    function startGame() {
+        $scope.tabCard = generateCards();
+        $rootScope.startTime();
+    }
+
+    function winGame() {
+        $rootScope.message = "Zwycięstwo!";
+        $rootScope.mesVisible = true;
+        $rootScope.assemble();
+        $rootScope.stopTime();
+    }
+
+    $scope.clicked = function (posX, posY, event) {
+        if ($rootScope.gameOn == false) startGame();
+
+        var openedBefore = false;
+        $scope.pairs.forEach(function (pair) {
+            if (pair.posX == posX && pair.posY == posY) {
+                openedBefore = true;
+                return;
+            }
+        });
+
+        if ($scope.block == true) return;
+        if (openedBefore) return;
+
+        if ($scope.lastChoice != undefined) {
+            if (!($scope.lastChoice.posX == posX && $scope.lastChoice.posY == posY)) {
+                event.target.src = 'img/' + $scope.tabCard[posX][posY] + '.jpg';
+                if ($scope.lastChoice.imageSrc == 'img/' + $scope.tabCard[posX][posY] + '.jpg') {
+                    $scope.pairs.push({ posX, posY });
+                    $scope.pairs.push({ posX: $scope.lastChoice.posX, posY: $scope.lastChoice.posY });
+                    $scope.numberOfPairs++;
+                    if ($scope.numberOfPairs == 8) winGame();
+                    $scope.lastChoice = undefined;
+                } else {
+                    $scope.block = true;
+                    $timeout(function () {
+                        $scope.block = false;
+                        event.target.src = `img/0.jpg`;
+                        $scope.lastChoice.target.src = `img/0.jpg`;
+                        $scope.lastChoice = undefined;
+                    }, 500);
+                }
+            }
+        } else {
+            $scope.lastChoice = {
+                posX: posX,
+                posY: posY,
+                imageSrc: 'img/'+$scope.tabCard[posX][posY]+'.jpg',
+                target: event.target
+            }
+            event.target.src = 'img/'+$scope.tabCard[posX][posY]+'.jpg';
+        }
+    }
+});
+
+gamefieldController.directive('gamefield', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'directives/gamefield/gamefield.html'
+    }
+});
---
+++ directives/time/time.js
@@ -0,0 +1,66 @@
+﻿var timeController = app.controller("timeController", function ($scope, $location, $interval, $timeout, $rootScope) {
+    $scope.timeChosen = parseInt($location.$$url.split("/")[1].split("sec")[0]); // 30, 60, 90
+    $scope.max = $scope.timeChosen * 1000;
+
+    $scope.setTime = function (newTime) {
+        $scope.timeChosen = newTime;
+    }
+
+    $rootScope.startTime = timeStart;
+    $rootScope.stopTime = disableTime;
+
+    var dateOfStart;
+
+    function timeStart() {
+        if ($rootScope.gameOn == false) {
+            dateOfStart = new Date().getTime() + $scope.max;
+            $rootScope.gameOn = true;
+            $scope.timeInterval = $interval(function () {
+                if ($rootScope.gameOn == true) {
+                    var actualDate = new Date(dateOfStart - new Date().getTime());
+
+                    var millisecond, second, minute;
+                    millisecond = actualDate.getMilliseconds();
+                    second = actualDate.getSeconds();
+                    minute = actualDate.getMinutes();
+
+                    if (millisecond < 10) millisecond = "00" + millisecond;
+                    else if (millisecond >= 10 && millisecond < 100) millisecond = "0" + millisecond;
+                    if (second < 10) second = "0" + second;
+                    if (minute < 10) minute = "0" + minute;
+
+                    if (dateOfStart - new Date().getTime() <= 0) {
+                        millisecond = "000";
+                        second = "00";
+                        minute = "00";
+                        timesUp();
+                    }
+
+                    $scope.czasik = minute + ":" + second + "." + millisecond;
+                    $scope.value = dateOfStart - new Date().getTime();
+                }
+            }, 1);
+        }
+    }
+
+    function timesUp() {
+        $rootScope.message = "Czas spadł! Przegrałeś!";
+        $rootScope.mesVisible = true;
+        $rootScope.assemble();
+        disableTime();
+    }
+
+    function disableTime() {
+        $interval.cancel($scope.timeInterval);
+        dateOfSTart = undefined;
+        $rootScope.reset();
+        $rootScope.gameOn = false;
+    }
+});
+
+timeController.directive('time', function () {
+    return {
+        templateUrl: 'directives/time/time.html',
+        restricts: 'A'
+    };
+});
